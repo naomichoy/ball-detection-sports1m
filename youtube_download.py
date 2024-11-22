f39c452ae32116040cf73e309be5f660f2c8d683
naomichoy <41099656+naomichoy@users.noreply.github.com>
2024-11-22 23:31:29 +0000

    change mapping method

--- youtube_download.py
+++ youtube_download.py
@@ -30,15 +30,16 @@ with open(LABELS_FILE, 'r', encoding='utf-8') as f:
 # check if target label is valid
 # create folder for each sport in TARGET_LABEL_NAME
 # map each text label to index
-target_label_index = []
+target_label_map = {}
 for lb in TARGET_LABEL_NAME:
     if lb in labels:
         OUTPUT_DIR = os.path.join(current_directory, "video_dataset", lb)    # Directory to save the videos
         os.makedirs(OUTPUT_DIR, exist_ok=True)
         ind = labels.index(lb)
-        target_label_index.append(ind)
+        target_label_map[ind] = lb
         print(f"Target label '{lb}' corresponds to index {ind}.")
     else:
+        print(f"Fail to map Target label '{lb}'.")
         TARGET_LABEL_NAME.pop(lb)
 
 
@@ -58,15 +59,25 @@ with open(DATASET_FILE, 'r') as f:
         split_label = label.split(',')
         for label in split_label:
             label = int(label)  # Convert label to integer
-            if label in target_label_index:
-                if label not in filtered_videos_by_class:
-                    filtered_videos_by_class[label] = [url]
+            if label in target_label_map.keys():
+                sport_cls = target_label_map[label]
+                if sport_cls not in filtered_videos_by_class: # first instance
+                    filtered_videos_by_class[sport_cls] = [url]
                 else:
-                    filtered_videos_by_class[label].append(url)
+                    filtered_videos_by_class[sport_cls].append(url)
                 urls += 1
         if urls > numFiles:
             break
 print("filtered_videos_by_class:", json.dumps(filtered_videos_by_class, indent=4))
+
+
+# Configure YoutubeDL
+ydl_opts = {
+    'format': 'best',
+    'outtmpl': os.path.join(OUTPUT_DIR, '%(id)s.%(ext)s'),  # Save with video ID as filename
+    'quiet': False,  # Set to True to suppress download logs
+    'noplaylist': True,
+}
 
 
 
